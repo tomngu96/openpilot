56e92b56aa4b214a9c84d12596cdd6abd0a9989a
Babyhamsta <22938086+Babyhamsta@users.noreply.github.com>
2024-07-31 15:37:40 -0500

    Update interface.py
    Adjusted accord_11g more to cut down on ping pong

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -128,7 +128,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.HONDA_ACCORD_11G:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.24]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.4], [0.24]]
       
     elif candidate == CAR.HONDA_ACCORD:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
