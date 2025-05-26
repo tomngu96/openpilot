fdc9f957c449f8869351029bb84ca82016688456
Tom <166699995+tomngu96@users.noreply.github.com>
2025-05-26 02:46:35 -0700

    revert

--- selfdrive/car/honda/interface.py
+++ selfdrive/car/honda/interface.py
@@ -130,21 +130,8 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.8], [0.24]]
 
     elif candidate == CAR.HONDA_ACCORD_11G:
-      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 12288], [0, 12288]]
-      # TODO TOM Working here
-      ret.lateralTuning.pid.kf = 0.00005
-
-      # stock kiV is a constant .18
-      ret.lateralTuning.pid.kiBP = [0., 15.6464, 150.]
-      # basic tuning says to tune kf and kp with ki set to 0. it was originally .18
-      ret.lateralTuning.pid.kiV = [0.23, 0.13, 0.18]
-
-      # tom's experiment - we want to update the proportional gain (kp) for ~35mph and under. everything is in m/s so 15.6464 m/s
-      # 3 break points 0-15.6464 is a linear interp, 15.6464 - inf is maintained as existing
-      # stock kpV is a constant .6. last known kpv without osciollation on old lower torque was .2
-      ret.lateralTuning.pid.kpBP = [0., 15.6464, 150.]
-      ret.lateralTuning.pid.kpV = [0.03, 0.17, 0.23]
-
+      ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.2], [0.18]]
     
     elif candidate == CAR.HONDA_ACCORD:
       ret.lateralParams.torqueBP, ret.lateralParams.torqueV = [[0, 4096], [0, 4096]]  # TODO: determine if there is a dead zone at the top end
